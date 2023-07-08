b4c899624ba305b82c9d1270c60c006e2f39cfa5
Luke Ramljak <lukeramljak@proton.me>
2023-07-08 15:28:10 +0800

    Update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,34 +1,13 @@
 local wezterm = require("wezterm")
 
-local function font_with_fallback(name, params)
-	local names = { name, "Apple Color Emoji", "azuki_font" }
-	return wezterm.font_with_fallback(names, params)
-end
-
-local font_name = "JetBrainsMonoNL NF"
-
 return {
 	-- OpenGL for GPU acceleration, Software for CPU
 	front_end = "OpenGL",
 
-	color_scheme = 'Catppuccin Mocha',
+	color_scheme = "Catppuccin Mocha",
 
 	-- Font config
-	font = font_with_fallback(font_name),
-	font_rules = {
-		{
-			italic = true,
-			font = font_with_fallback(font_name, { italic = true }),
-		},
-		{
-			italic = false,
-			font = font_with_fallback(font_name, { bold = true }),
-		},
-		{
-			intensity = "Bold",
-			font = font_with_fallback(font_name, { bold = true }),
-		},
-	},
+	font = wezterm.font("JetBrainsMono NF"),
 	warn_about_missing_glyphs = false,
 	font_size = 12,
 	line_height = 1.0,
@@ -120,11 +99,11 @@ return {
 			mods = "CTRL|SHIFT",
 			action = wezterm.action({ ActivateTabRelative = -1 }),
 		}, -- standard copy/paste bindings
-		-- {
-		-- 	key = "x",
-		-- 	mods = "CTRL",
-		-- 	action = "ActivateCopyMode",
-		-- },
+		{
+			key = "x",
+			mods = "CTRL",
+			action = "ActivateCopyMode",
+		},
 		{
 			key = "v",
 			mods = "CTRL|SHIFT",
@@ -158,5 +137,4 @@ return {
 	inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 },
 	window_background_opacity = 0.8,
 	window_close_confirmation = "NeverPrompt",
-  	window_frame = { active_titlebar_bg = "#45475a", font = font_with_fallback(font_name, { bold = true }) },
 }
