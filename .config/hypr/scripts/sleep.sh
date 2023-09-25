7d83010fb1a6fc5b2ba03d7f85e9cd901099fa01
Luke Ramljak <lukeramljak@proton.me>
2023-09-25 11:37:32 +0800

    update hyprland config

--- .config/hypr/scripts/sleep.sh
+++ .config/hypr/scripts/sleep.sh
@@ -1,4 +1,3 @@
 swayidle -w timeout 600 'hyprctl dispatch dpms off' \
             timeout 900 'systemctl suspend' \
-            resume 'hyprctl dispatch dpms on' \
-            before-sleep 'swaylock -f -c 000000' &
+            resume 'hyprctl dispatch dpms on' &
