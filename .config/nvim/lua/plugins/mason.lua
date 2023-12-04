34003d46bef8ec8e9b355b9c5dbf2b7840be79ad
Luke ramljak <lukeramljak@proton.me>
2023-12-04 19:36:41 +0800

    add mason config

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "eslint-lsp",
+      "json-lsp",
+      "lua-language-server",
+      "prettier",
+      "typescript-language-server"
+    }
+  }
+}
