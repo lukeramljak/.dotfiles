bd705b3e07166ad13f9ba63da5b441a13992e476
Luke Ramljak <lukeramljak@proton.me>
2023-12-13 20:44:30 +0800

    add tailwind

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,16 +1,19 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "eslint-lsp",
-      "json-lsp",
-      "html-lsp",
-      "css-lsp",
-      "emmet-ls",
-      "lua-language-server",
-      "prettier",
-      "typescript-language-server",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "eslint-lsp",
+        "json-lsp",
+        "html-lsp",
+        "css-lsp",
+        "emmet-ls",
+        "lua-language-server",
+        "prettier",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+      },
     },
   },
 }
