62ce790d69dee672e39a8fc05b41463f4ef353b3
Luke Ramljak <lukeramljak@proton.me>
2023-12-13 20:43:33 +0800

    add tailwind plugin

---
+++ .config/nvim/lua/plugins/tailwind-sorter.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "laytan/tailwind-sorter.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+    build = "cd formatter && npm i && npm run build",
+    config = true,
+    opts = {
+      on_save_enabled = true,
+      on_save_pattern = { "*.html", "*.js", "*.jsx", "*.tsx", "*.twig", "*.hbs", "*.php", "*.heex", "*.astro" },
+    },
+  },
+}
