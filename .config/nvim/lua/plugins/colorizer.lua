7808ddf587311dfb02acb812d84481841db468ba
Luke Ramljak <lukeramljak@proton.me>
2023-12-13 20:44:03 +0800

    fix colorizer

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,3 +1,14 @@
 return {
   "NvChad/nvim-colorizer.lua",
+  opts = {
+    filetypes = {
+      "css",
+      "javascript",
+      "html",
+    },
+    user_default_options = {
+      css = true,
+      tailwind = true,
+    },
+  },
 }
