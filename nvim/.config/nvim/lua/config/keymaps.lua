e4bd73febe45ae86ed2a8351e29a22affa7f9007
Luke Ramljak <lukeramljak@proton.me>
2024-01-07 19:25:32 +0800

    update keymaps/included extras

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
+vim.keymap.set("n", "<leader>fl", vim.cmd.Ex, { desc = "Open netrw" })
