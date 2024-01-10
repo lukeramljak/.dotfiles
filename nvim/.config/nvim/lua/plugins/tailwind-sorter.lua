f47213fe12c5403e2c7483e03cb6ce12e53b1eac
Luke ramljak <lukeramljak@proton.me>
2024-01-10 22:38:12 +0800

    fix plugin not formatting

--- nvim/.config/nvim/lua/plugins/tailwind-sorter.lua
+++ nvim/.config/nvim/lua/plugins/tailwind-sorter.lua
@@ -4,7 +4,9 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
     build = "cd formatter && npm i && npm run build",
     config = true,
-    on_save_enabled = true,
-    on_save_pattern = { "*.html", "*.js", "*.jsx", "*.tsx", "*.twig", "*.hbs", "*.php", "*.heex", "*.astro" },
+    opts = {
+      on_save_enabled = true,
+      on_save_pattern = { "*.html", "*.js", "*.jsx", "*.tsx", "*.twig", "*.hbs", "*.php", "*.heex", "*.astro" },
+    }
   },
 }
