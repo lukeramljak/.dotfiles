0554c6f6fa1b298d2d399586abbce1283531e892
Luke Ramljak <lukeramljak@proton.me>
2024-01-29 20:08:30 +0800

    remove oh-my-zsh

---
+++ zsh/.config/zsh/keybinds.zsh
@@ -0,0 +1,16 @@
+##
+## Keybindings
+##
+
+bindkey '^[[H' beginning-of-line
+bindkey '^[[F' end-of-line
+bindkey -s '^K' 'ls^M'
+bindkey -s '^o' '_smooth_fzf^M'
+
+# fix backspace and other stuff in vi-mode
+bindkey -M viins '\e/' _vi_search_fix
+bindkey "^?" backward-delete-char
+bindkey "^H" backward-delete-char
+bindkey "^U" backward-kill-line
+
+# vim:ft=zsh:nowrap
