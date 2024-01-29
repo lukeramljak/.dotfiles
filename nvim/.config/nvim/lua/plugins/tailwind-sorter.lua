8a396f596057c3a7dffebcdfc0d405f7f68eb7f3
Luke Ramljak <lukeramljak@proton.me>
2024-01-29 23:08:46 +0800

    remove default lazy configs

--- nvim/.config/nvim/lua/plugins/tailwind-sorter.lua
+++ nvim/.config/nvim/lua/plugins/tailwind-sorter.lua
@@ -6,7 +6,6 @@ return {
     config = true,
     opts = {
       on_save_enabled = true,
-      on_save_pattern = { "*.html", "*.js", "*.jsx", "*.tsx", "*.twig", "*.hbs", "*.php", "*.heex", "*.astro" },
     }
   },
 }
