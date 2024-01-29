bf51c482c9ca9429d7b08d08312887a5b250d7ad
Luke Ramljak <lukeramljak@proton.me>
2024-01-29 23:08:56 +0800

    update aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,16 +2,20 @@
 ## Aliases
 ##
 
+alias shutdown="sudo shutdown now"
+alias restart="sudo reboot"
+alias suspend="sudo pm-suspend"
+
 alias c="clear"
 alias q="exit"
 alias cleanram="sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'"
 alias trim_all="sudo fstrim -va"
-alias mkgrub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
-alias mtar='tar -zcvf' # mtar <archive_compress>
-alias utar='tar -zxvf' # utar <archive_decompress> <file_list>
-alias z='zip -r' # z <archive_compress> <file_list>
-alias uz='unzip' # uz <archive_decompress> -d <dir>
-alias sr='source ~/.config/zsh/env.zsh'
+alias mkgrub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias mtar="tar -zcvf" # mtar <archive_compress>
+alias utar="tar -zxvf" # utar <archive_decompress> <file_list>
+alias z="zip -r" # z <archive_compress> <file_list>
+alias uz="unzip" # uz <archive_decompress> -d <dir>
+alias sr="source ~/.config/zsh/env.zsh"
 alias ..="cd .."
 alias psg="ps aux | grep -v grep | grep -i -e VSZ -e" 
 alias mkdir="mkdir -p"
@@ -22,189 +26,43 @@ alias la="ls -a"
 alias lla="ls -la"
 alias lt="ls --tree"
 alias cat="bat --color always --plain"
-alias grep='grep --color=auto'
-alias mv='mv -v'
-alias cp='cp -vr'
-alias rm='rm -vr'
+alias grep="grep --color=auto"
+alias mv="mv -v"
+alias cp="cp -vr"
+alias rm="rm -vr"
 
-alias commit="git add . && git commit -m"
-alias push="git push"
-alias git-rm="git ls-files --deleted -z | xargs -0 git rm"
-alias g=git
-alias ga='git add'
-alias gaa='git add --all'
-alias gam='git am'
-alias gama='git am --abort'
-alias gamc='git am --continue'
-alias gams='git am --skip'
-alias gamscp='git am --show-current-patch'
-alias gap='git apply'
-alias gapa='git add --patch'
-alias gapt='git apply --3way'
-alias gau='git add --update'
-alias gav='git add --verbose'
-alias gb='git branch'
-alias gbD='git branch -D'
-alias gba='git branch -a'
-alias gbd='git branch -d'
-alias gbda='git branch --no-color --merged | command grep -vE "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)" | command xargs git branch -d 2>/dev/null'
-alias gbl='git blame -b -w'
-alias gbnm='git branch --no-merged'
-alias gbr='git branch --remote'
-alias gbs='git bisect'
-alias gbsb='git bisect bad'
-alias gbsg='git bisect good'
-alias gbsr='git bisect reset'
-alias gbss='git bisect start'
-alias gc='git commit -v'
-alias 'gc!'='git commit -v --amend'
-alias gca='git commit -v -a'
-alias 'gca!'='git commit -v -a --amend'
-alias gcam='git commit -a -m'
-alias 'gcan!'='git commit -v -a --no-edit --amend'
-alias 'gcans!'='git commit -v -a -s --no-edit --amend'
-alias gcas='git commit -a -s'
-alias gcasm='git commit -a -s -m'
-alias gcb='git checkout -b'
-alias gcd='git checkout $(git_develop_branch)'
-alias gcf='git config --list'
-alias gclr='git clone --recurse-submodules'
-alias gcl='git clone'
-alias gcld='git clone --depth'
-alias gclean='git clean -id'
-alias gcm='git checkout $(git_main_branch)'
-alias gcmsg='git commit -m'
-alias 'gcn!'='git commit -v --no-edit --amend'
-alias gco='git checkout'
-alias gcor='git checkout --recurse-submodules'
-alias gcount='git shortlog -sn'
-alias gcp='git cherry-pick'
-alias gcpa='git cherry-pick --abort'
-alias gcpc='git cherry-pick --continue'
-alias gcs='git commit -S'
-alias gcsm='git commit -s -m'
-alias gcss='git commit -S -s'
-alias gcssm='git commit -S -s -m'
-alias gd='git diff'
-alias gdca='git diff --cached'
-alias gdct='git describe --tags $(git rev-list --tags --max-count=1)'
-alias gdcw='git diff --cached --word-diff'
-alias gds='git diff --staged'
-alias gdt='git diff-tree --no-commit-id --name-only -r'
-alias gdup='git diff @{upstream}'
-alias gdw='git diff --word-diff'
-alias gf='git fetch'
-alias gfa='git fetch --all --prune --jobs=10'
-alias gfg='git ls-files | grep'
-alias gfo='git fetch origin'
-alias gg='git gui citool'
-alias gga='git gui citool --amend'
-alias ggpull='git pull origin "$(git_current_branch)"'
-alias ggpur=ggu
-alias ggpush='git push origin "$(git_current_branch)"'
-alias ggsup='git branch --set-upstream-to=origin/$(git_current_branch)'
-alias ghh='git help'
-alias gignore='git update-index --assume-unchanged'
-alias gignored='git ls-files -v | grep "^[[:lower:]]"'
-alias git-svn-dcommit-push='git svn dcommit && git push github $(git_main_branch):svntrunk'
-alias gk='\gitk --all --branches &!'
-alias gke='\gitk --all $(git log -g --pretty=%h) &!'
-alias gl='git pull'
-alias glg='git log --stat'
-alias glgg='git log --graph'
-alias glgga='git log --graph --decorate --all'
-alias glgm='git log --graph --max-count=10'
-alias glgp='git log --stat -p'
-alias glo='git log --oneline --decorate'
-alias globurl='noglob urlglobber '
-alias glod='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'\'
-alias glods='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'\'' --date=short'
-alias glog='git log --oneline --decorate --graph'
-alias gloga='git log --oneline --decorate --graph --all'
-alias glol='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'
-alias glola='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'' --all'
-alias glols='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'' --stat'
-alias glp=_git_log_prettily
-alias glum='git pull upstream $(git_main_branch)'
-alias gm='git merge'
-alias gma='git merge --abort'
-alias gmom='git merge origin/$(git_main_branch)'
-alias gmtl='git mergetool --no-prompt'
-alias gmtlvim='git mergetool --no-prompt --tool=vimdiff'
-alias gmum='git merge upstream/$(git_main_branch)'
-alias gp='git push'
-alias gpd='git push --dry-run'
-alias gpf='git push --force-with-lease'
-alias 'gpf!'='git push --force'
-alias gpoat='git push origin --all && git push origin --tags'
-alias gpr='git pull --rebase'
-alias gpristine='git reset --hard && git clean -dffx'
-alias gpsup='git push --set-upstream origin $(git_current_branch)'
-alias gpu='git push upstream'
-alias gpv='git push -v'
-alias gr='git remote'
-alias gra='git remote add'
-alias grb='git rebase'
-alias grwh='git rm --cached `git ls-files -i -c --exclude-from=.gitignore`'
-alias grwhx='git ls-files -i -c --exclude-from=.gitignore | xargs git rm --cached'
-alias grad='git rm -r --cached . && git add .'
-alias grba='git rebase --abort'
-alias grbc='git rebase --continue'
-alias grbd='git rebase $(git_develop_branch)'
-alias grbi='git rebase -i'
-alias grbm='git rebase $(git_main_branch)'
-alias grbo='git rebase --onto'
-alias grbom='git rebase origin/$(git_main_branch)'
-alias grbs='git rebase --skip'
-alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
-alias grev='git revert'
-alias grh='git reset'
-alias grhh='git reset --hard'
-alias grm='git rm'
-alias grmc='git rm --cached'
-alias grmv='git remote rename'
-alias groh='git reset origin/$(git_current_branch) --hard'
-alias grrm='git remote remove'
-alias grs='git restore'
-alias grset='git remote set-url'
-alias grss='git restore --source'
-alias grst='git restore --staged'
-alias grt='cd "$(git rev-parse --show-toplevel || echo .)"'
-alias gru='git reset --'
-alias grup='git remote update'
-alias grv='git remote -v'
-alias gsb='git status -sb'
-alias gsd='git svn dcommit'
-alias gsh='git show'
-alias gsi='git submodule init'
-alias gsps='git show --pretty=short --show-signature'
-alias gsr='git svn rebase'
-alias gss='git status -s'
-alias gst='git status'
-alias gsta='git stash push'
-alias gstaa='git stash apply'
-alias gstall='git stash --all'
-alias gstc='git stash clear'
-alias gstd='git stash drop'
-alias gstl='git stash list'
-alias gstp='git stash pop'
-alias gsts='git stash show --text'
-alias gstu='gsta --include-untracked'
-alias gsu='git submodule update'
-alias gsw='git switch'
-alias gswc='git switch -c'
-alias gswd='git switch $(git_develop_branch)'
-alias gswm='git switch $(git_main_branch)'
-alias gtl='gtl(){ git tag --sort=-v:refname -n -l "${1}*" }; noglob gtl'
-alias gts='git tag -s'
-alias gtv='git tag | sort -V'
-alias gunignore='git update-index --no-assume-unchanged'
-alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
-alias gup='git pull --rebase'
-alias gupa='git pull --rebase --autostash'
-alias gupav='git pull --rebase --autostash -v'
-alias gupv='git pull --rebase -v'
-alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
-alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip-- [skip ci]"'
+alias vim="nvim"
+
+alias tmuxk="tmux kill-session -t"
+alias tmuxa="tmux attach -t"
+alias tmuxl="tmux list-sessions"
+
+alias gs="git status"
+alias gss="git status -s"
+alias ga="git add"
+alias gp="git push"
+alias gpraise="git blame"
+alias gpo="git push origin"
+alias gpof="git push origin --force-with-lease"
+alias gpofn="git push origin --force-with-lease --no-verify"
+alias gpt="git push --tag"
+alias gtd="git tag --delete"
+alias gtdr="git tag --delete origin"
+alias grb="git branch -r" # display remote branch
+alias gplo="git pull origin"
+alias gb="git branch"
+alias gc="git commit"
+alias gd="git diff"
+alias gco="git checkout"
+alias gl="git log --pretty=oneline"
+alias gr="git remote"
+alias grs="git remote show"
+alias glol="git log --graph --abbrev-commit --oneline --decorate"
+alias gclean="git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 git branch -d" # Delete local branch merged with master
+alias gblog="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:red)%(refname:short)%(color:reset) - %(color:yellow)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:blue)%(committerdate:relative)%(color:reset))'"                                                             # git log for each branches
+alias gsub="git submodule update --remote"                                                        # pull submodules
+alias gj="git-jump"                                                                               # Open in vim quickfix list files of interest (git diff, merged...)
+
+alias dif="git diff --no-index"                                                                   # Diff two files even if not in git repo! Can add -w (don't diff whitespaces)
 
 # vim:ft=zsh
