c58b791c61aa642ba44f2a93e0441d5cf7d077d7
Luke Ramljak <lukeramljak@proton.me>
2024-02-07 20:50:02 +0800

    add hyprlang

--- nvim/.config/nvim/lua/luke/plugins/init.lua
+++ nvim/.config/nvim/lua/luke/plugins/init.lua
@@ -2,4 +2,5 @@ return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"folke/trouble.nvim",
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"luckasRanarison/tree-sitter-hyprlang", -- syntax highlighting for hyprland files
 }
