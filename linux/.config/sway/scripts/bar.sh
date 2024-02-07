de8c5466d96b1a2d32ed6dd03d545b194c644a5d
Luke Ramljak <lukeramljak@proton.me>
2024-02-07 15:11:23 +0800

    update config

--- linux/.config/sway/scripts/bar.sh
+++ linux/.config/sway/scripts/bar.sh
@@ -4,8 +4,10 @@ while true
 do
     disk=$(df -h / | awk 'NR==2{print $5}')
     vol=$(pamixer --get-volume)
+    default_sink=$(pactl get-default-sink)
+    output=$(pactl list sinks | grep -A1 "Name: $default_sink" | grep 'Description' | cut -d ':' -f2 | xargs)
     date=$(date +'%Y-%m-%d %I:%M:%S %p')
 
-    echo "disk: $disk | vol: $vol% | $date |"
+    echo "disk: $disk | vol: $vol% | $output | $date |"
     sleep 1
 done
