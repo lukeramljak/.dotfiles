0c894b9beb3e4ac758119e30d162797d19e6ee97
Luke Ramljak <lukeramljak@proton.me>
2024-02-08 13:29:50 +0800

    update dashboard

--- nvim/.config/nvim/lua/luke/plugins/alpha-nvim.lua
+++ nvim/.config/nvim/lua/luke/plugins/alpha-nvim.lua
@@ -1,10 +1,12 @@
 return {
 	"goolord/alpha-nvim",
 	event = "VimEnter",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
+		local fn = vim.fn
+		local section = dashboard.section
+		local config = dashboard.config
 
 		-- Set header
 		dashboard.section.header.val = {
@@ -27,16 +29,25 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("e", "  > new file", "<cmd>ene<CR>"),
+			dashboard.button("<leader> ff", "󰱼  > find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("<leader> fs", "  > find word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("<leader> wr", "󰁯  > restore session for cwd", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "  > quit nvim", "<cmd>qa<CR>"),
+		}
+
+		local marginTopPercent = 0.2
+		local headerPadding = fn.max({ 2, fn.floor(fn.winheight(0) * marginTopPercent) })
+
+		config.layout = {
+			{ type = "padding", val = headerPadding },
+			section.header,
+			{ type = "padding", val = 2 },
+			section.buttons,
 		}
 
 		-- Send config to alpha
-		alpha.setup(dashboard.opts)
+		alpha.setup(dashboard.opts, config)
 
 		-- Disable folding on alpha buffer
 		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
