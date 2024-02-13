5367ae8399d23be00a288e6791cd54d609d28850
Luke Ramljak <lukeramljak@proton.me>
2024-02-13 18:20:51 +0800

    remove plugin

--- nvim/.config/nvim/lua/luke/plugins/init.lua
+++ nvim/.config/nvim/lua/luke/plugins/init.lua
@@ -2,5 +2,4 @@ return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"folke/trouble.nvim",
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-	"luckasRanarison/tree-sitter-hyprlang", -- syntax highlighting for hyprland files
 }
