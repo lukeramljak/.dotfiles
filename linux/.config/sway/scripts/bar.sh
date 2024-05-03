0f4ac323253e51039538c825bc7a2710beac1401
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-05-04 00:13:22 +0800

    Remove linux configs

--- linux/.config/sway/scripts/bar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-disk=$(df -h / | awk 'NR==2{print $5}')
-vol=$(pamixer --get-volume)
-default_sink=$(pactl get-default-sink)
-output=$(pactl list sinks | grep -A1 "Name: $default_sink" | grep 'Description' | cut -d ':' -f2 | xargs)
-date=$(date +'%Y-%m-%d %I:%M:%S %p')
-
-echo "disk: $disk | vol: $vol% | $output | $date "
