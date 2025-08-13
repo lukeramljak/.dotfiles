e467e3cfb6f6158ddcd08839e11effdf49d3179f
Luke Ramljak <lukeramljak@proton.me>
2025-08-13 20:55:01 +0800

    remove mini.hipatterns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -173,19 +173,6 @@ vim.keymap.set("n", "<leader>e", function()
   files.open(vim.api.nvim_buf_get_name(0), true)
 end)
 
-require("mini.hipatterns").setup({
-  highlighters = {
-    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-
-    -- Highlight hex color strings (`#rrggbb`) using that color
-    hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
-  },
-})
-
 require("mini.icons").setup()
 
 local notify = require("mini.notify")
