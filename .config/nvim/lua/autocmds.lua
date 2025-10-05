da300d2fec1f88ee00b4f27cba6818f7fe620d2e
Luke Ramljak <lukeramljak@proton.me>
2025-10-05 10:03:07 +0800

    refactor(nvim): update autocmds

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,13 +1,24 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
-  group = vim.api.nvim_create_augroup("user_highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("lukeramljak/yank_highlight", { clear = true }),
+  desc = "Highlight on yank",
   callback = function()
     vim.hl.on_yank()
   end,
 })
 
+vim.api.nvim_create_autocmd("VimResized", {
+  group = vim.api.nvim_create_augroup("lukeramljak/resize_splits", { clear = true }),
+  desc = "Resize splits on window resize",
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
 vim.api.nvim_create_autocmd("PackChanged", {
-  group = vim.api.nvim_create_augroup("user_treesitter-updated", { clear = true }),
+  group = vim.api.nvim_create_augroup("lukeramljak/treesitter_updated", { clear = true }),
+  desc = "Update TS parsers when updating plugin",
   ---@param event {data: {kind: "install" | "update" | "delete", path: string, spec: vim.pack.Spec}}
   callback = function(event)
     if event.data.spec.name == "nvim-treesitter" and event.data.kind == "update" then
@@ -15,14 +26,5 @@ vim.api.nvim_create_autocmd("PackChanged", {
         vim.cmd("TSUpdate")
       end)
     end
-  end,
-})
-
-vim.api.nvim_create_autocmd("VimResized", {
-  group = vim.api.nvim_create_augroup("resize_splits", { clear = true }),
-  callback = function()
-    local current_tab = vim.fn.tabpagenr()
-    vim.cmd("tabdo wincmd =")
-    vim.cmd("tabnext " .. current_tab)
   end,
 })
