c89223661652de958c8b893e56e2d9ba89b86925
Luke Ramljak <lukeramljak@proton.me>
2025-10-05 10:57:39 +0800

    refactor(nvim): shorten diagnostic virtual text

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -17,7 +17,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
 
     if client:supports_method(methods.textDocument_documentHighlight) then
-      local under_cursor_highlights_group = vim.api.nvim_create_augroup("user_cursor_highlights", { clear = false })
+      local under_cursor_highlights_group =
+        vim.api.nvim_create_augroup("lukeramljak/cursor_highlights", { clear = false })
       vim.api.nvim_create_autocmd({ "CursorHold", "InsertLeave" }, {
         group = under_cursor_highlights_group,
         desc = "Highlight references under the cursor",
@@ -36,27 +37,35 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 vim.diagnostic.config({
   severity_sort = true,
-  float = { border = "rounded", source = "if_many" },
   underline = { severity = vim.diagnostic.severity.ERROR },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = diagnostic_icons.ERROR,
-      [vim.diagnostic.severity.WARN] = diagnostic_icons.WARN,
-      [vim.diagnostic.severity.INFO] = diagnostic_icons.INFO,
-      [vim.diagnostic.severity.HINT] = diagnostic_icons.HINT,
-    },
-  },
+  signs = false,
   virtual_text = {
-    source = "if_many",
     spacing = 2,
     format = function(diagnostic)
-      local diagnostic_message = {
-        [vim.diagnostic.severity.ERROR] = diagnostic.message,
-        [vim.diagnostic.severity.WARN] = diagnostic.message,
-        [vim.diagnostic.severity.INFO] = diagnostic.message,
-        [vim.diagnostic.severity.HINT] = diagnostic.message,
+      -- Use shorter, nicer names for some sources:
+      local special_sources = {
+        ["Lua Diagnostics."] = "lua",
+        ["Lua Syntax Check."] = "lua",
       }
-      return diagnostic_message[diagnostic.severity]
+
+      local message = diagnostic_icons[vim.diagnostic.severity[diagnostic.severity]]
+      if diagnostic.source then
+        message = string.format("%s %s", message, special_sources[diagnostic.source] or diagnostic.source)
+      end
+      if diagnostic.code then
+        message = string.format("%s[%s]", message, diagnostic.code)
+      end
+
+      return message .. " "
+    end,
+  },
+  float = {
+    source = "if_many",
+    -- Show severity icons as prefixes
+    prefix = function(diag)
+      local level = vim.diagnostic.severity[diag.severity]
+      local prefix = string.format(" %s ", diagnostic_icons[level])
+      return prefix, "Diagnostic" .. level:gsub("^%l", string.upper)
     end,
   },
 })
