c1ce69e9eb267de14de50bd6fc734290dee6b200
Luke Ramljak <lukeramljak@proton.me>
2025-10-05 09:53:38 +0800

    feat(nvim): use dprint for prettier formatting

---
+++ .config/nvim/lsp/dprint.lua
@@ -0,0 +1,5 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "dprint", "lsp" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "jsonc", "graphql" },
+}
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -22,11 +22,19 @@ require("catppuccin").setup({
 vim.cmd.colorscheme("catppuccin-mocha")
 
 require("conform").setup({
+  notify_on_error = false,
+  notify_no_formatters = false,
   formatters_by_ft = {
-    go = { "gofmt", "goimports" },
+    go = { "gopls" },
+    json = { "prettier", name = "dprint" },
+    jsonc = { "prettier", name = "dprint" },
     lua = { "stylua" },
-    typescript = { "prettier" },
-    typescriptreact = { "prettier" },
+    markdown = { "prettier" },
+    typescript = { "prettier", name = "dprint", timeout_ms = 500 },
+    typescriptreact = { "prettier", name = "dprint", timeout_ms = 500 },
+    yaml = { "prettier" },
+    -- For filetypes without a formatter:
+    ["_"] = { "trim_whitespace", "trim_newlines" },
   },
   format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
   formatters = {
