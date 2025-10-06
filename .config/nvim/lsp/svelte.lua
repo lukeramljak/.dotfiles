7fb6d69b697cc3e6d16b615aaebc83c0ad165479
Luke Ramljak <lukeramljak@proton.me>
2025-10-06 21:53:41 +0800

    fix(nvim): fix svelte crashing on save

--- .config/nvim/lsp/svelte.lua
+++ .config/nvim/lsp/svelte.lua
@@ -13,6 +13,14 @@
 return {
   cmd = { "svelteserver", "--stdio" },
   filetypes = { "svelte" },
+  handlers = {
+    ["workspace/semanticTokens/refresh"] = function()
+      return vim.NIL
+    end,
+    ["workspace/inlayHint/refresh"] = function()
+      return vim.NIL
+    end,
+  },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
     -- Svelte LSP only supports file:// schema. https://github.com/sveltejs/language-tools/issues/2777
