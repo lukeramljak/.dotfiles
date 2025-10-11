7573dacc4de281f3e4970aeb639c206464bbfc6e
Luke Ramljak <lukeramljak@proton.me>
2025-10-11 11:14:41 +0800

    feat(nvim): add css TS parser and disable unknown at rules

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "go", "json", "lua", "markdown", "svelte", "tsx", "typescript" },
+  ensure_installed = { "css", "go", "json", "html", "lua", "markdown", "svelte", "tsx", "typescript" },
   auto_install = true,
   highlight = { enable = true },
   indent = { enable = true },
