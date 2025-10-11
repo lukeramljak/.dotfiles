907b2411e5c9ae0760d4359fbb76abec9f78f63e
Luke Ramljak <lukeramljak@proton.me>
2025-10-11 10:08:01 +0800

    feat(nvim): add biome

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,20 +2,24 @@ require("conform").setup({
   notify_on_error = false,
   notify_no_formatters = false,
   formatters_by_ft = {
+    css = { "biome" },
     go = { "gopls" },
-    json = { "prettier", name = "dprint" },
-    jsonc = { "prettier", name = "dprint" },
+    javascript = { "biome", "prettier" },
+    json = { "biome", "prettier" },
+    jsonc = { "biome", "prettier" },
     lua = { "stylua" },
-    markdown = { "prettier" },
-    typescript = { "prettier", name = "dprint", timeout_ms = 500 },
-    typescriptreact = { "prettier", name = "dprint", timeout_ms = 500 },
+    markdown = { "biome", "prettier" },
+    svelte = { "biome", "prettier" },
+    typescript = { "biome", "prettier" },
+    typescriptreact = { "biome", "prettier" },
     yaml = { "prettier" },
     -- For filetypes without a formatter:
     ["_"] = { "trim_whitespace", "trim_newlines" },
   },
   format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
   formatters = {
-    -- Require a Prettier configuration file to format.
+    -- Require a configuration file to format.
+    biome = { require_cwd = true },
     prettier = { require_cwd = true },
   },
 })
