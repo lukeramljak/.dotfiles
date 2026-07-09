78cfa9199fd2f26b7626b7392cf02fa3c1d653cc
Luke Ramljak <lukeramljak@proton.me>
2026-07-09 11:02:01 +0800

    chore(fish): remove devbox config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -42,13 +42,6 @@ fish_config theme choose catppuccin-mocha --color-theme=dark
 
 fzf --fish | source
 
-if test (hostname) = devbox
-    oh-my-posh init fish --config $HOME/.config/ohmyposh/robbyrussell.json | source
-    abbr -a bye "sudo systemctl suspend"
-else
-    oh-my-posh init fish --config $HOME/.config/ohmyposh/tokyonight_storm.json | source
-    abbr -a wake "wakeonlan 18:C0:4D:8F:A9:8B"
-    abbr -a devbox-sleep "ssh devbox 'sudo systemctl suspend'"
-end
+oh-my-posh init fish --config $HOME/.config/ohmyposh/tokyonight_storm.json | source
 
 zoxide init fish --cmd cd | source
