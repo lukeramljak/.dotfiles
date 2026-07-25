32f725381b99c6cec2892bf58c6196d67a553fb8
Luke Ramljak <lukeramljak@proton.me>
2026-07-25 15:24:32 +0800

    chore(nvim): remove blink max items

--- .config/nvim/plugin/blink.lua
+++ .config/nvim/plugin/blink.lua
@@ -68,7 +68,6 @@ add({
         list = {
           -- Insert items while navigating the completion list
           selection = { preselect = false, auto_insert = true },
-          max_items = 10,
         },
         documentation = { auto_show = true },
         menu = {
